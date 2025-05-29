57d894b21a9c6fa40e5d87381271a4a3dee4c303
zarif-webflow <zarif.webflow@gmail.com>
2025-05-29 20:51:47 +0600

    add missing entrypoint file, remove unnecessary info from package.json

---
+++ entypoints.mjs
@@ -0,0 +1,5 @@
+/**
+ * This file defines the entry points for the application.
+ * Example: ['src/target_folder/target_file.ts', 'src/target_folder2/target_file2.ts']
+ */
+export const ENTRY_POINTS = ["src/index.ts"];
