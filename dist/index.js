3994e6d51e84f11a6c644668cde82c51622f4fff
zarif-webflow <zarif.webflow@gmail.com>
2025-05-30 13:53:36 +0600

    remove version from package.json

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,8 @@
-window.Webflow||(window.Webflow=[]);window.Webflow.push(()=>{});
+// bin/live-reload.js
+new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+// src/index.ts
+window.Webflow ||= [];
+window.Webflow.push(() => {
+});
+//# sourceMappingURL=index.js.map
